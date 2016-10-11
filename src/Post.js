ed3301679fbf76e9e2b184281c960d0643c72cf2
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-10-12 00:01:04 +0300

    Add Props & Data Mapping to Posts

--- src/Post.js
+++ src/Post.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 export default class Post extends Component {
   render() {
     return (
-      <h2>Cупер новость</h2>
+      <div>
+        <div>{this.props.name}</div>
+        <div>{this.props.description}</div>
+      </div>
     );
   }
 }
+
+Post.propTypes = {
+  id: React.PropTypes.number.isRequired,
+  name: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string,
+};
--- src/app.js
+++ src/app.js
@@ -3,10 +3,19 @@ import Post from './Post';
 import PostForm from './PostForm';
 
 export default class App extends Component {
+  renderPosts() {
+    return this.props.posts.map(post => {
+      return (
+        <Post key={post.id} id={post.id} name={post.name} description={post.description} />
+      );
+    });
+  }
   render() {
     return (
       <div>
-        <Post />
+        <div>
+          {this.renderPosts()}
+        </div>
         <PostForm />
       </div>
     );
