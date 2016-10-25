c18ce8744f44a3c522ffad678aa1625ecc3be255
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-10-25 11:39:07 +0300

    Rename App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import App from './pp';
+import App from './App';
 import configureStore from './store/configureStore'
 
 const store = configureStore()
